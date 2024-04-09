224558a1d78a638113d5094d79762129188e4814
Vijay Singh <dprophecyguy@gmail.com>
2024-04-09 23:51:54 +0530

    Completed \d flag support

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,25 @@ use std::env;
 use std::io;
 use std::process;
 
+fn contains_digits(s: &str) -> bool {
+    s.chars().any(|c| c.is_digit(10))
+}
+
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
-    } else {
-        panic!("Unhandled pattern: {}", pattern)
     }
+
+    if pattern == "\\d" {
+        return contains_digits(input_line);
+    }
+
+    panic!("Unhandled pattern: {}", pattern)
 }
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     if env::args().nth(1).unwrap() != "-E" {
         println!("Expected first argument to be '-E'");
         process::exit(1);
